fa6f9920d9a713525567e9975830dc3a9863fc36
Brian Zalewski <brian@enviedsolutions.com>
2018-02-11 00:28:14 -0500

    Commit.

--- src/components/pool-config-table/pool-config-table.ts
+++ src/components/pool-config-table/pool-config-table.ts
@@ -5,7 +5,7 @@ import { Component, Input } from '@angular/core';
   templateUrl: 'pool-config-table.html'
 })
 export class PoolConfigTableComponent {
-  @Input('poolConfigs') parseCookieValue: any;
+  @Input('poolConfigs') pool: any;
 
   constructor() {}
 
